9ce27f6985b31f6f404cbf5ead72ba0e58c6d34d
Steve South <southsystems@gmail.com>
2013-11-09 13:31:49 +0000

    Add some subtle rotation to the page turn

--- js/main.js
+++ js/main.js
@@ -25,7 +25,7 @@
 		Slide.prototype._position = function() {
 			for (var i = 0, l = this.pages.length; i < l; i++) {
 				if (i > this.selected) {
-					this._hideToRight(this.pages[i]);
+					this._hideRight(this.pages[i]);
 				} else if (i === this.selected) {
 					this._show(this.pages[i]);
 				} else {
@@ -35,26 +35,26 @@
 		}
 
 		Slide.prototype._fadeOut = function(page) {
-			page.style.webkitTransform = "translate(0,0) scale(0.9,0.9)";
+			page.style.webkitTransform = "translate(0,0) scale(0.9,0.95) rotate(-3deg)";
 			page.style.opacity = "0.7";
 		};
 
 		Slide.prototype._show = function(page) {
-			page.style.webkitTransform = "translate(0,0) scale(1,1)";
+			page.style.webkitTransform = "translate(0,0) scale(1,1) rotate(0deg)";
 			page.style.opacity = "1";
 		};
 
-		Slide.prototype._hideToRight = function(page) {
-			page.style.webkitTransform = "translate(100%,0) scale(1,1)";
-			page.style.opacity = "1";
+		Slide.prototype._hideRight = function(page) {
+			page.style.webkitTransform = "translate(100%,0) scale(1,1) rotate(0deg)";
+			page.style.opacity = "0.9";
 		};
 
 		Slide.prototype._initControls = function(element) {
-			$(element).find('.previous').on("click", function() {
+			$(element).find('.previous').on("touchstart", function() {
 				this.previous();
 			}.bind(this));
 
-			$(element).find('.next').on("click", function() {
+			$(element).find('.next').on("touchstart", function() {
 				this.next();
 			}.bind(this));
 		};
